f6efd8094adea63856b9f944ded69cc02da1f31b
gzucconi <zucconi.g@edificarseguros.com.ar>
2019-12-17 13:54:49 -0300

    add

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -30,6 +34,7 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+   
     {
       resolve: `gatsby-source-github-api`,
       options: {
@@ -51,8 +56,6 @@ module.exports = {
         }`,
       },
     },
-    require('dotenv').config({
-      path: `.env.${process.env.NODE_ENV}`,
-    })
   ],
+  
 }
